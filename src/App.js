2cddc6609877ab967c926e12b875df9d8680c981
Akshat Soni <65664731+akshatsoni8055@users.noreply.github.com>
2022-10-04 18:56:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react';
 import { Drawer } from './container';
 import configureStore from './store';
 const store = configureStore();
-
+console.log("envs \n", process.env);
 function App() {
   return (
     <Provider store={store}>
